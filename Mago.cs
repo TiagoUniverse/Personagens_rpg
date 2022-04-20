d1f8adc5f2c6550ed24b2565cc5fd2759bbbd2f4
Tiago Cesar da Silva Lopes <tiagolopes@pe.senac.br>
2022-04-20 09:52:48 -0300

    Testes da criação da repetição

--- Mago.cs
+++ Mago.cs
@@ -5,11 +5,62 @@ namespace Personagens_rpg
     class Mago : Ser_vivo
     {
 
+        private string feitiço1;
+        private string tipo_mago;
+        private string feitiço2;
 
+        public string getTipo_mago()
+        {
+            return tipo_mago;
+        }
+
+        public string getFeitiço1()
+        {
+            return feitiço1;
+        }
+
+        public string getFeitiço2()
+        {
+            return feitiço2;
+        }
+
+        public void setTipo_mago(string tipo_mago)
+        {
+            this.tipo_mago = tipo_mago;
+        }
+
+        public void setFeitiço1(string feitiço1)
+        {
+            this.feitiço1 = feitiço1;
+        }
+
+        public void setFeitiço2(string feitiço2)
+        {
+            this.feitiço2 = feitiço2;
+        }
+
+
+        public void lançar_feitiço1()
+        {
+            if (tipo_mago == null   || feitiço1 == null || feitiço2 == null   )
+            {
+                Console.WriteLine("Perai, aventureiro! Antes de ir pra luta, tem algo faltando saber sobre você!");
+                Console.WriteLine("diga: que tipo de mago você é? ");
+                setTipo_mago(Console.ReadLine());
+
+                Console.WriteLine("Selecione seu 1ª feitiço ofensivo: ");
+                setFeitiço1(Console.ReadLine());
+                
+                Console.WriteLine("Selecione seu 2ª feitiço ofensivo: ");
+                setFeitiço2(Console.ReadLine());
+                
+            }
+
+            Console.WriteLine("Toma essa! Feitiço: " + getFeitiço1());
+
+        }
 
         
-
-
 
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,11 +4,22 @@ namespace Personagens_rpg
 {
     class Program
     {
+      //  static public int decisao;
         static void Main(string[] args)
         {
-            Mago mago1 = new Mago();
+         //   Mago mago1 = new Mago();
 
-            mago1.Socar();
+           // mago1.Socar();
+
+            //mago1.lançar_feitiço1();
+
+            int decisao;
+
+             do
+           {
+               Console.WriteLine("oie");
+               decisao = Convert.ToInt32(Console.ReadLine());
+           } while (decisao == 0);
         }
     }
 }
